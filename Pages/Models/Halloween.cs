ac9432c6f4dafd0992bdb8b3cc1db10e54074d45
Eris <fitzgeralderis@gmail.com>
2021-11-13 16:10:42 -0500

    first try

---
+++ Pages/Models/Halloween.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+
+namespace halloweenParty.Models
+{
+    public class Home
+    {
+        public int ID {    get; set;   }    
+        //primary key for the database
+        [Required]
+        public string name {   get; set;   } = string.Empty;
+        public int age {   get; set;   }
+        public int arrivalTime {  get; set;   }
+        public bool parking {   get; set;   }
+    }
+}
